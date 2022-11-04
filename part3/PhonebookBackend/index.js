78ac50d98443f200c6b25a4098a51d67adef26ff
Anh Duy Tran <anh.2.tran@tuni.fi>
2022-11-04 13:03:46 +0200

    first deployed fullstack web app

--- part3/PhonebookBackend/index.js
+++ part3/PhonebookBackend/index.js
@@ -1,7 +1,9 @@
 const express = require('express')
 const morgan = require('morgan')
+const cors = require('cors')
 
-const PORT = 3001
+
+
 const app = express()
 
 let persons = [
@@ -32,6 +34,8 @@ const getNewId = () => {
   return Math.floor(Math.random() * maxId)
 }
 
+app.use(cors())
+app.use(express.static('build'))
 app.use(express.json())
 app.use(morgan((tokens, req, res) => {
   return [
@@ -101,4 +105,7 @@ const unknownEndpoint = (req, res) => {
 
 app.use(unknownEndpoint)
 
-app.listen(PORT)
+const PORT = process.env.PORT || 3001
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`)
+})
